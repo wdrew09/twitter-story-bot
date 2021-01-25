743e0cdebef36d5f5a3c783f0f1836f8ca0f4b88
Andrew Wilson <awi279@uky.edu>
2021-01-25 17:20:49 -0500

    adding dynamo features, varied progress

---
+++ awstest.js
@@ -0,0 +1,142 @@
+const AWS = require('aws-sdk');
+const config = require('./config.js');
+
+AWS.config.update(config.aws_remote_config);
+let docClient = new AWS.DynamoDB.DocumentClient();
+
+let fetchOneByKey = function (message) {
+    var params = {
+        TableName: "main_table",
+        Key: {
+            "message": message
+        }
+        // KeyConditionExpression: 'message = :message'
+    };
+    docClient.scan(params, function (err, data) {
+        if (err) {
+            console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+        }
+        else {
+            console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+        }
+    })
+}
+
+let addToUsedTable = function (message, author) {
+
+    var input = {
+        "message": "test message2",
+        "created_by": "placeholder",
+        "created_on": new Date().toString(),
+    };
+    var params = {
+        TableName: "used_messages",
+        Item: input
+    };
+    docClient.put(params, function (err, data) {
+
+        if (err) {
+            console.log("users::save::error - " + JSON.stringify(err, null, 2));
+        } else {
+            console.log("users::save::success");
+        }
+    });
+}
+
+// let getAllItemsFromUsed = function () {
+//     var params = {
+//         TableName: "used_messages",
+//     };
+//     docClient.scan(params, function (err, data) {
+//         if (err) {
+//             // console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+//         }
+//         else {
+//             // console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+//             return data;
+//         }
+//     })
+// }
+
+let addToMainTable = function (message, author) {
+
+    var input = {
+        "message": "test message2",
+        "created_by": "placeholder",
+        "created_on": new Date().toString(),
+    };
+    var params = {
+        TableName: "main_table",
+        Item: input
+    };
+    docClient.put(params, function (err, data) {
+
+        if (err) {
+            console.log("users::save::error - " + JSON.stringify(err, null, 2));
+        } else {
+            console.log("users::save::success");
+        }
+    });
+}
+
+// let getAllItemsFromMain = async function () {
+//     var params = {
+//         TableName: "main_table",
+//     };
+//     return  docClient.scan(params, function (err, data) {
+//         if (err) {
+//             // console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+//         }
+//         else {
+//             // console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+//             console.log("hey", data)
+//             return data
+//         }
+//     })
+
+// }
+
+let getAllItemsFromMain = function () {
+    var params = {
+        TableName: "main_table",
+    };
+    return new Promise((resolve, reject) => {
+        docClient.scan(params, function (err, data) {
+            if (err) {
+                // console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+                reject(err)
+            }
+            else {
+                // console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+                // console.log("hey", data)
+                resolve(data)
+            }
+        })
+    })
+}
+
+let getAllItemsFromUsed = function () {
+    var params = {
+        TableName: "used_messages",
+    };
+    return new Promise((resolve, reject) => {
+        docClient.scan(params, function (err, data) {
+            if (err) {
+                // console.log("users::fetchOneByKey::error - " + JSON.stringify(err, null, 2));
+                reject(err)
+            }
+            else {
+                // console.log("users::fetchOneByKey::success - " + JSON.stringify(data, null, 2));
+                resolve(data)
+            }
+        })
+    })
+}
+
+module.exports = {
+    fetchOneByKey,
+    addToMainTable,
+    addToUsedTable,
+    getAllItemsFromMain,
+    getAllItemsFromUsed
+}
